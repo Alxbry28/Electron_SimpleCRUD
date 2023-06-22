316297d170d83d23e3ed2770e50f29d78cf63e1e
Alex Bryan Q. Arellano <AQArellano@ABS-CBN.COM>
2023-06-22 21:12:41 +0800

    [LEARN] Learning about renders from codevolution

--- main.js
+++ main.js
@@ -28,12 +28,27 @@ const createWindow = () => {
         }
     });
 
-    win.loadFile('index.html');
+    // win.webContents.openDevTools();
+    // win.loadFile('index.html');
+    win.loadFile(path.join(__dirname,'./windows/one.html'));
+
+    const win2 = new BrowserWindow({
+        width: 800,
+        height: 600
+    
+    });
+
+    // win2.webContents.openDevTools();
+    // win2.loadFile('index.html');
+    win2.loadFile(path.join(__dirname,'./windows/two.html'));
+
 };
 
 app.whenReady().then(() => {
     createWindow();
 });
+
+
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
